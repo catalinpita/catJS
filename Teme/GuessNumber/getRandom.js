8157862c72b9ec1e155f6f66e0f4e65730bd70d4
catalinpita <55147973+catalinpita@users.noreply.github.com>
2019-11-18 00:01:40 +0200

    ghiceste numarul!!!

--- Teme/GuessNumber/getRandom.js
+++ Teme/GuessNumber/getRandom.js
@@ -1,49 +1,148 @@
 
 var isStarted = false;
 
+var maxTries, upperRange;
+var numberToGuess;
+var triedNumbers;
+var tryNumber;
+
 var maxTriesInput = document.getElementById('maxTries');
 var upperRangeInput = document.getElementById('upperRange');
 
+var currentTryInput = document.getElementById('currentTry');
+
 var startButton = document.getElementById('startButton');
+var tryButton = document.getElementById('tryButton');
+var cheatButton = document.getElementById('cheatButton');
 
 startButton.addEventListener("click", startFunction);
 
+tryButton.addEventListener("click", tryFunction);
+cheatButton.addEventListener("click", cheatFunction);
 
 
-var maxTries = 0;
-var upperRange = 0;
+initFunction();
 
-var numberToGuess;
-
-
-
-// var inputWidth = document.getElementById('width');
-// var elementResult = document.getElementById('result');
-
-// var form = document.querySelector('.rectangle-form');
-
-// form.addEventListener('submit', function(evt) {
-
-//   var length = inputLength.value || 0;
-//   var width = inputWidth.value || 0;
-//   var result = 0;
-
-//   result = length * width;
-//   elementResult.innerText = result;
-//   evt.preventDefault();
-// });
-
+/**********************************************************************************************/
+// numberToGuess = Math.random() * (upperRange + 1); //(Math.random() * ((max - min) + 1)) + min.
 function startFunction(evt) {
-
-
   if (isStarted === false) {
-    // numberToGuess = Math.random() * (upperRange + 1); //(Math.random() * ((max - min) + 1)) + min
-    startButton.classList.add('disabled');
-    startButton.disabled = true;
+
+    maxTries = maxTriesInput.value;
+    upperRange = parseInt(upperRangeInput.value);
+    numberToGuess = Math.ceil(Math.random() * upperRange);
+    triedNumbers=[];
     isStarted = true;
-    alert(isStarted);
-  } else {
-    alert('Ignored');
+    tryNumber=1;
+    document.getElementById('response').innerHTML = '';
+    document.getElementById('numberOfTry').innerText = '1';
+    document.getElementById('yourTries').innerText = '';
+    
+    enableControlsByState(isStarted);
+
   }
   evt.preventDefault();
+}
+
+/**********************************************************************************************/
+function tryFunction(evt) {
+
+  const myTry = parseInt(currentTryInput.value);
+
+  if (isStarted === true) {
+    if (myTry > upperRange) {
+      alert(`Psssst ma gandesc la un numar intre 0 si ${upperRange}.`);
+
+    } else if (notTriedYet(triedNumbers, myTry)) {
+      if (myTry === numberToGuess) {
+        document.getElementById('response').innerHTML = `Bravo, ai ghicit! <b>${myTry}</b> este numarul la care m-am gandit.`;
+        isStarted = false;
+        enableControlsByState(isStarted);
+      } else {
+        document.getElementById('response').innerHTML = `Numarul <b>${myTry}</b> este mai <b>${(myTry < numberToGuess) ? 'mic' : 'mare'}</b> decat cel la care ma gandesc.`;
+
+        if (++tryNumber > maxTries) {
+          document.getElementById('response').innerHTML = `Ai terminat munitia!!! eu ma gandeam la numarul ${numberToGuess}.`;
+          document.getElementById('numberOfTry').innerText = '...';
+
+          isStarted = false;
+          enableControlsByState(isStarted);
+        }
+        triedNumbers.push(myTry);
+      }
+      document.getElementById('numberOfTry').innerText = tryNumber;
+      document.getElementById('yourTries').innerText = triedNumbers;
+    } else {
+      alert(`Ai mai incercat numarul ${myTry}.`);
+    }
+  }
+
+  evt.preventDefault();
+}
+
+/**********************************************************************************************/
+function notTriedYet(triedNumbers, myTry) {
+  for (index = 0; index < triedNumbers.length; index++)
+    if (myTry === parseInt(triedNumbers[index]))
+      return false;
+  return true;
+}
+
+/**********************************************************************************************/
+function cheatFunction(evt) {
+
+  if (isStarted == true) {
+  alert(numberToGuess);
+    // isStarted = false;
+    // enableControlsByState(isStarted)
+  }
+  evt.preventDefault();
+}
+
+/**********************************************************************************************/
+function initFunction() {
+
+  isStarted = false;
+  maxTries = 0;
+  upperRange = 0;
+  numberToGuess = 0;
+  triedNumbers = [];
+  tryNumber = 1;
+ 
+  enableControlsByState(isStarted);
+  document.getElementById('response').innerHTML = '';
+  document.getElementById('numberOfTry').innerText = '1';
+  document.getElementById('yourTries').innerHTML = '';
+
+}
+
+/**********************************************************************************************/
+function enableControlsByState(state) {
+  if (state == false) {
+    startButton.classList.remove('disabled');
+    startButton.disabled = false;
+
+    tryButton.classList.add('disabled');
+    tryButton.disabled = true;
+
+    cheatButton.classList.add('disabled');
+    cheatButton.disabled = true;
+
+    currentTry.disabled = true;
+    maxTriesInput.disabled = false;
+    upperRangeInput.disabled = false;
+  } else {
+    startButton.classList.add('disabled');
+    startButton.disabled = true;
+
+    tryButton.classList.remove('disabled');
+    tryButton.disabled = false;
+
+    cheatButton.classList.remove('disabled');
+    cheatButton.disabled = false;
+
+    currentTry.disabled = false;
+    maxTriesInput.disabled = true;
+    upperRangeInput.disabled = true;
+  }
 }
