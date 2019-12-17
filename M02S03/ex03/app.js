f661bbcfa0755bd4fd7fae444272e5fd4a27df1f
catalinpita <55147973+catalinpita@users.noreply.github.com>
2019-12-17 19:59:46 +0200

    soaricele

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,14 @@
 
+let stage = document.querySelector('.stage');
+let message = document.querySelector('.message');
+let countElement = document.querySelector('.count');
+let count = 0;
+stage.addEventListener('mouseover', () => {
+  message.innerText = 'Soarecele e pe scena';
+  count++;
+});
+
+stage.addEventListener('mouseout', () => {
+  message.innerText = 'Soarecele nu e pe scena';
+  countElement.innerText = `Soaricele a fost pe scena de ${count} ori.`;
+});
