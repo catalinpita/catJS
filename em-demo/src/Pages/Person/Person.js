48dd0cb7c3eecdc480fbf789ceb56dbed4e15e42
catalinpita <55147973+catalinpita@users.noreply.github.com>
2020-01-09 20:30:39 +0200

    gata?

--- em-demo/src/Pages/Person/Person.js
+++ em-demo/src/Pages/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {  Component } from 'react';
+import React, { Component } from 'react';
 
 export class Person extends Component {
   constructor(props) {
@@ -9,12 +9,34 @@ export class Person extends Component {
       surname: 'BBB',
       ager: 31,
       skills: ['html', 'js']
-    }
+    };
   }
+  removeSkill = (skillIndex) => {
+    let arr = [...this.state.skills.splice(0, skillIndex), ...this.state.skills.splice(skillIndex + 1)];
+    this.setState({
+      skills: arr
+    });
+  }
+
   render() {
     return (
       <div className="container">
-
+        <h1>Person ({this.state.name + ' ' + this.state.surname})</h1>
+        {
+          this.state.skills.length ?
+            <ul>
+              {
+                this.state.skills.map((skill, i) => {
+                  return (
+                    <li key={`skill-${i}`}>{skill}
+                      <button onClick={() => { this.removeSkill(i) }}>-</button>
+                    </li>
+                  );
+                })
+              }
+            </ul> :
+            <p>N-are</p>
+        }
       </div>
     );
   }
